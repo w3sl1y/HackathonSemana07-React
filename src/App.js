36f198cbc6154de4ce378c863cb0c71235ac39a7
w3sl1y <SV73623892@idat.edu.pe>
2021-10-09 09:52:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div class="header">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
