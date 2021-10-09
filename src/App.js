cadee34fbb8bdbf521aa9f4693ef0716dd4dbe42
w3sl1y <SV73623892@idat.edu.pe>
2021-10-09 11:34:01 -0500

    actualizacion

--- src/App.js
+++ src/App.js
@@ -9,13 +9,23 @@ function App() {
           <a>MOFISH</a>
         </div>
         <div class="menu">
+          <div class="each">
           <a>IN THEATHER</a>
+          </div>
+          <div class="each">
           <a>COMING SOON</a>
+          </div>
+          <div class="each">
           <a>MIDNIGHT</a>
+          </div>
         </div>
         <div class="search">
-          <input placeholder="Search"/>
+          <div class="eachs">
+            <input placeholder="Search"/>
+          </div>
+          <div class="eachs">
           <img src="https://cdn.icon-icons.com/icons2/2483/PNG/512/user_icon_149851.png" height="40px" width="40px"></img>
+          </div>
         </div>
       </div>
     </div>
