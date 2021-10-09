f162636da2a558795f9bea4113fb15ef928673d7
w3sl1y <SV73623892@idat.edu.pe>
2021-10-09 11:05:19 -0500

    actualizacion

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ function App() {
     <div className="container">
       <div class="header">
         <div class="logo">
-          <h1>MOFISH</h1>
+          <a>MOFISH</a>
         </div>
         <div class="menu">
-          <h1>MOFISH</h1>
-          <h1>MOFISH</h1>
-          <h1>MOFISH</h1>
+          <a>IN THEATHER</a>
+          <a>COMING SOON</a>
+          <a>MIDNIGHT</a>
+        </div>
+        <div class="search">
+          <input placeholder="Search"/>
+          <img src="https://cdn.icon-icons.com/icons2/2483/PNG/512/user_icon_149851.png" height="40px" width="40px"></img>
         </div>
       </div>
     </div>
