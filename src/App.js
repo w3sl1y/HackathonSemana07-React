7e887ec388084fa196eb7b0dd271f48cec3153c9
w3sl1y <SV73623892@idat.edu.pe>
2021-10-09 12:21:57 -0500

    actualizacion

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ function App() {
     <div className="container">
       <div class="header">
         <div class="logo">
-          <a>MOFISH</a>
+        <div class="cam">
+          <img src="https://cdn-icons-png.flaticon.com/512/173/173450.png" height="30px" width="30px"/>
+          </div>
+          <div class="mofish">
+          <a href="#">MOFISH</a>
+          </div>
         </div>
         <div class="menu">
           <div class="each">
-          <a>IN THEATHER</a>
+          <a href="#">IN THEATHER</a>
           </div>
           <div class="each">
-          <a>COMING SOON</a>
+          <a href="#">COMING SOON</a>
           </div>
           <div class="each">
-          <a>MIDNIGHT</a>
+          <a href="#">MIDNIGHT</a>
           </div>
         </div>
         <div class="search">
           <div class="eachs">
-            <input placeholder="Search"/>
+            <input placeholder="Search..."/>
           </div>
           <div class="eachs">
-          <img src="https://cdn.icon-icons.com/icons2/2483/PNG/512/user_icon_149851.png" height="40px" width="40px"></img>
+          <img src="https://cdn.icon-icons.com/icons2/2483/PNG/512/user_icon_149851.png" height="40px" width="40px"/>
           </div>
         </div>
       </div>
