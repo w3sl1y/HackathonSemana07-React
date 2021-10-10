583e757437d087b8cdfb011076c7ed1b6df95bbe
w3sl1y <SV73623892@idat.edu.pe>
2021-10-10 16:10:47 -0500

    actualización

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './assets/css/App.css';
+import Content from './components/content';
 
 function App() {
   return (
@@ -14,25 +15,28 @@ function App() {
           </div>
         </div>
         <div class="menu">
-          <div class="each">
+          <div class="each1">
           <a href="#">IN THEATHER</a>
           </div>
-          <div class="each">
+          <div class="each2">
           <a href="#">COMING SOON</a>
           </div>
-          <div class="each">
+          <div class="each3">
           <a href="#">MIDNIGHT</a>
           </div>
         </div>
         <div class="search">
           <div class="eachs">
-            <input placeholder="Search..."/>
+            <input placeholder="Search...    🔎"/>
           </div>
           <div class="eachs">
-          <img src="https://cdn.icon-icons.com/icons2/2483/PNG/512/user_icon_149851.png" height="40px" width="40px"/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png" height="30px" width="30px"/>
           </div>
         </div>
       </div>
+      <Content/>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,160L48,176C96,192,192,224,288,240C384,256,480,256,576,229.3C672,203,768,149,864,133.3C960,117,1056,139,1152,165.3C1248,192,1344,224,1392,240L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
     </div>
   );
 }
---
+++ src/components/content.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+
+class Content extends Component{
+    render(){
+        return (
+            <div class="content">
+        <div class="poster">
+          <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5a80c996-2710-4a92-8814-344169c0bdbf/d9wxypd-a85982a7-9553-4ee6-a3b5-0583c689f2de.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVhODBjOTk2LTI3MTAtNGE5Mi04ODE0LTM0NDE2OWMwYmRiZlwvZDl3eHlwZC1hODU5ODJhNy05NTUzLTRlZTYtYTNiNS0wNTgzYzY4OWYyZGUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.x_1NLFr-ync7R2YXfay1gTrMKITT267BiYPjz8lAxnU" height="460px" width="330px"/>
+        </div>
+        <div class="description">
+          <div class="description1">
+            <a href="#">Finding Dory</a>
+          </div>
+          <div class="description2">
+            <div class="description2-1">
+            <a href="#">ANIMATION</a><a>•</a>
+            <a href="#">ADVENTURE</a><a>•</a>
+            <a href="#">COMEDY</a>
+            </div>
+            <div class="description2-2">
+            <p>8/10</p><a href="#">★</a><a href="#">★</a><a href="#">★</a><a href="#">★</a><a href="#">★</a>
+            </div>
+          </div>
+          <div class="description3">
+          <a>“Finding Dory” reunites the friendly-but-forgetful blue tang fish with her loved ones, 
+             and<br/>everyone learns a few things about the true meaning of family along the way. 
+             The all-new big-<br/>screen aventure dives into theaters now...</a>
+          </div>
+          <div class="description4">
+            <button class="ticket">GET TICKET</button>
+            <button class="play">▶</button>
+            <button class="heart">♥</button>
+          </div>
+        </div>
+      </div>
+        );
+    }
+    }
+    export default Content;
